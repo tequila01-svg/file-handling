907273c101f320a83b3bc427095a6da6477c837f
tequila01-svg <onifadeayobami008@gmail.com>
2024-11-15 17:35:52 +0100

    modification

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ file=open('sample.txt')
 #read file
 print(file.read())
 file.close()
+
+
+#open in write mode
+file_write=open('sample.txt','w')
+file_write.write("hello, my name is Ayobami")
+#print|file_write.read()
+file_write.close()
